ab456b7fe1994ad7ee376184fff9506f12b50603
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2015-04-17 14:04:13 -0400

    Remove gluPerspective: use our own implementation

--- KillBox/src/Camera.java
+++ KillBox/src/Camera.java
@@ -14,133 +14,143 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.util.glu.GLU.*;
 
 public class Camera
 {
-    Player Plyr;
+	Player Plyr;
 
-    // Postions inside the camera are as represented in OpenGL
-    // The axis are inverted, thus a positive value is a negative value
-    private float PosX;
-    private float PosY;
-    private float PosZ;
-    private float RotX;
-    private float RotY;
-    private float RotZ;
+	// Postions inside the camera are as represented in OpenGL
+	// The axis are inverted, thus a positive value is a negative value
+	private float PosX;
+	private float PosY;
+	private float PosZ;
+	private float RotX;
+	private float RotY;
+	private float RotZ;
 
-    private float FOV;
-    private float Aspect;
-    private float Near;
-    private float Far;
+	private float FOV;
+	private float Aspect;
+	private float Near;
+	private float Far;
 
-    public Camera(Player Plyr, float FOV, float Aspect, float Near, float Far)
-    {
-        this.Plyr = Plyr;
+	public Camera(Player Plyr, float FOV, float Aspect, float Near, float Far)
+	{
+		this.Plyr = Plyr;
 
-        PosX = Plyr.PosY;
-        PosY = Plyr.PosZ + Plyr.ViewZ;   // Internally, the player's Z is the height.
-        PosZ = Plyr.PosX;   // But in OpenGL, Z is vertical like the internal Y.
-        RotX = 0;
-        RotY = Plyr.GetDegreeAngle();
-        RotZ = 0;
+		PosX = Plyr.PosY;
+		PosY = Plyr.PosZ + Plyr.ViewZ;   // Internally, the player's Z is the height.
+		PosZ = Plyr.PosX;   // But in OpenGL, Z is vertical like the internal Y.
+		RotX = 0;
+		RotY = Plyr.GetDegreeAngle();
+		RotZ = 0;
 
-        this.FOV = FOV;
-        this.Aspect = Aspect;
-        this.Near = Near;
-        this.Far = Far;
-        InitProjection();
-    }
+		this.FOV = FOV;
+		this.Aspect = Aspect;
+		this.Near = Near;
+		this.Far = Far;
+		InitProjection();
+	}
 
-    private void InitProjection()
-    {
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluPerspective(FOV,Aspect,Near,Far);
-        glMatrixMode(GL_MODELVIEW);
-    }
+	// Sets the perspective without glu, so let's call it glPerspective. 
+	private void glPerspective(float FOV, float Aspect, float Near, float Far)
+	{
+		// This replaces 'gluPerspective'. 
+		float FH = Math.tan(FOV / 360 * Math.PI) * Near;
+		float FW = FH * Aspect;
+	
+		// Sets the Frustum to perspective mode. 
+		glFrustum( -FW, FW, -FH, FH, Near, Far );
+	}
 
-    public void UseView()
-    {
-        //glRotatef(RotX, 1, 0, 0);
-        glRotatef(RotY, 0, -1, 0);
-        //glRotatef(RotZ, 0, 0, 1);
-        //glTranslatef(PosX, PosY, PosZ);
-    }
+	private void InitProjection()
+	{
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glPerspective(FOV, Aspect, Near, Far);
+		glMatrixMode(GL_MODELVIEW);
+	}
 
-    public void UpdateCamera()
-    {
-        PosX = Plyr.PosY;
-        PosY = Plyr.PosZ + Plyr.ViewZ;
-        PosZ = Plyr.PosX;
-        RotY = Plyr.GetDegreeAngle();
-    }
+	public void UseView()
+	{
+		//glRotatef(RotX, 1, 0, 0);
+		glRotatef(RotY, 0, -1, 0);
+		//glRotatef(RotZ, 0, 0, 1);
+		//glTranslatef(PosX, PosY, PosZ);
+	}
 
-    public void ChangeProperties(float FOV, float Aspect, float Near, float Far)
-    {
-        this.FOV = FOV;
-        this.Aspect = Aspect;
-        this.Near = Near;
-        this.Far = Far;
-    }
+	public void UpdateCamera()
+	{
+		PosX = Plyr.PosY;
+		PosY = Plyr.PosZ + Plyr.ViewZ;
+		PosZ = Plyr.PosX;
+		RotY = Plyr.GetDegreeAngle();
+	}
 
-    public float PosX()
-    {
-        return PosX;
-    }
+	public void ChangeProperties(float FOV, float Aspect, float Near, float Far)
+	{
+		this.FOV = FOV;
+		this.Aspect = Aspect;
+		this.Near = Near;
+		this.Far = Far;
+	}
 
-    public float PosY()
-    {
-        return -PosY;
-    }
+	public float PosX()
+	{
+		return PosX;
+	}
 
-    public float PosZ()
-    {
-        return PosZ;
-    }
+	public float PosY()
+	{
+		return -PosY;
+	}
 
-    public void PosX(float PosX)
-    {
-        this.PosX = PosX;
-    }
+	public float PosZ()
+	{
+		return PosZ;
+	}
 
-    public void PosY(float PosY)
-    {
-        this.PosY = -PosY;
-    }
+	public void PosX(float PosX)
+	{
+		this.PosX = PosX;
+	}
 
-    public void PosZ(float PosZ)
-    {
-        this.PosZ = PosZ;
-    }
+	public void PosY(float PosY)
+	{
+		this.PosY = -PosY;
+	}
 
-    public float RotX()
-    {
-        return RotX;
-    }
+	public void PosZ(float PosZ)
+	{
+		this.PosZ = PosZ;
+	}
 
-    public float RotY()
-    {
-        return RotY;
-    }
+	public float RotX()
+	{
+		return RotX;
+	}
 
-    public float RotZ()
-    {
-        return RotZ;
-    }
+	public float RotY()
+	{
+		return RotY;
+	}
 
-    public void RotX(float RotX)
-    {
-        this.RotX = RotX;
-    }
+	public float RotZ()
+	{
+		return RotZ;
+	}
 
-    public void RotY(float RotY)
-    {
-        this.RotY = RotY;
-    }
+	public void RotX(float RotX)
+	{
+		this.RotX = RotX;
+	}
 
-    public void RotZ(float RotZ)
-    {
-        this.RotZ = RotZ;
-    }
+	public void RotY(float RotY)
+	{
+		this.RotY = RotY;
+	}
+
+	public void RotZ(float RotZ)
+	{
+		this.RotZ = RotZ;
+	}
 }
