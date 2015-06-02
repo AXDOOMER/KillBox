cb4b555907b34e9beafd9bffde354ca519132f30
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2015-06-01 22:33:21 -0400

    Initial stuff added to the Menu subsystem

--- KillBox/src/Menu.java
+++ KillBox/src/Menu.java
@@ -15,6 +15,74 @@
 
 public class Menu
 {
+	boolean Active = false;		// Show or hide the menu
+
 	// Build arrays for the menus that can drop from the menu bar
+	Strings[] BarItems;
+	Strings[] GameItems;
+	Strings[] Options;
+	Strings[] Sound;
+	Strings[] Video;
+
+	// Don't mix 'Strings' with 'String'.
+	public enum Strings
+	{
+		Game, Option, Control, Sound, Video,
+		New_Game, Join_Game, Quit_Game, Play_Demo, About, Exit,
+		Use_Freelook, Show_Messages, Show_HUD, Show_Debug,
+		Grab_Mouse, Enable_Chat, Mouse_Sensitivity,
+		SFX_Volume, Mode, Two_D, Three_D, Duppler,
+		FullScreen, Filtering, View_Depth
+	}
+
+	// Screen coordinates are represented as a fixed resolution
+	int GridWidth = 640;
+	int GridHeight = 480;
+
+	final String HRchar = "--";		// Horizontal rule  (<hr /> in HTML)
+
+	public Menu()
+	{
+		BarItems = new Strings[5];
+		BarItems[0] = Strings.Game;
+		BarItems[1] = Strings.Option;
+		BarItems[2] = Strings.Control;
+		BarItems[3] = Strings.Sound;
+		BarItems[4] = Strings.Video;
+
+	}
+
+	public void DrawMenuItemText(Strings Item, int PosX, int PosY)
+	{
+		// String where item's text is stored
+		String Text = "NONE";
+
+		// Do a swtich-case to set the good text using the item's name
+
+
+
+		// Draw the text
+		// Use: DrawText(Text, PosX, PosY);
+	}
+
+	public void DrawText(String Text, int PosX, int PosY)
+	{
+
+	}
+
+	public boolean Active()
+	{
+		return Active;
+	}
+
+	public void Show()
+	{
+		Active = true;
+	}
+
+	public void Hide()
+	{
+		Active = false;
+	}
 
 }
