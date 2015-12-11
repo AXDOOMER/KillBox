310eb81a09caf8c26dda7719840dab3213886579
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2015-12-11 16:33:22 -0500

    New collision detection code for new map

--- KillBox/src/Player.java
+++ KillBox/src/Player.java
@@ -793,6 +793,36 @@ public class Player
 							continue;
 						}
 					}
+					else if (StartZ != EndZ)
+					{
+						float Lowest = Lvl.Planes.get(Plane).Vertices().get(2);
+						float Highest = Lvl.Planes.get(Plane).Vertices().get(2);
+
+						// Check each Z coordinate to get the highest and the lowest Z of a vertex
+						for (int Point = 2; Point < Lvl.Planes.get(Plane).Vertices().size(); Point += 3)
+						{
+							if (Lvl.Planes.get(Plane).Vertices().get(Point) <= Lowest)
+							{
+								Lowest = Lvl.Planes.get(Plane).Vertices().get(Point);
+							}
+							else if (Lvl.Planes.get(Plane).Vertices().get(Point) >= Highest)
+							{
+								Highest = Lvl.Planes.get(Plane).Vertices().get(Point);
+							}
+						}
+
+						// Compare to the player
+						if (Lowest >= this.PosZ() + this.Height() && Highest >= this.PosZ() + this.Height())
+						{
+							// The wall is far above the player
+							continue;
+						}
+						else if (Lowest <= this.PosZ() && Highest <= this.PosZ())
+						{
+							// The wall is below above the player
+							continue;
+						}
+					}
 
 					// Fix what the next algorithm is going to miss (exactly vertical or horizontal walls)
 					// Test for a vertical wall
