705ed8687d17b73ceea9690d0476cfe1208f8a80
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2016-10-20 16:15:08 -0400

    Better exception handling

--- KillBox/src/Level.java
+++ KillBox/src/Level.java
@@ -41,307 +41,300 @@ public class Level
 	{
 		Filter = PlaneFilter;
 		ShortestWall = Integer.MAX_VALUE;
+		String Line = "";
 
 		try
 		{
 			// Load from file
 			BufferedReader LevelFile = new BufferedReader(new FileReader(LvlName));
-			String Line;
 
-			try
+			while ((Line = LevelFile.readLine()) != null)
 			{
-				while ((Line = LevelFile.readLine()) != null)
+				if (Line.length() < 2 || Line.charAt(0) == '#')
 				{
-					if (Line.length() < 2 || Line.charAt(0) == '#')
+					// It's a comment. Get another line.
+					continue;
+				}
+
+				//Serious business
+				int Position = Line.length();
+				char[] Find = {':', ';', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+				// Find first of
+				for (int i = 0; i < 12; i++)
+				{
+					int Temp = Line.indexOf(Find[i]);
+
+					if (Temp < Position && Temp != -1)
 					{
-						// It's a comment. Get another line.
-						continue;
-					}
-
-					//Serious business
-					int Position = Line.length();
-					char[] Find = {':', ';', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-					// Find first of
-					for (int i = 0; i < 12; i++)
-					{
-						int Temp = Line.indexOf(Find[i]);
-
-						if (Temp < Position && Temp != -1)
-						{
-							Position = Temp;
-						}
-					}
-
-					String Content = Line.substring(0, Position);
-					String id;
-					int Number;
-
-					if (Line.contains("level:"))
-					{
-						while (!Line.contains("}"))
-						{
-							Line = LevelFile.readLine();
-
-							if (Line.contains("{"))
-							{
-								continue;
-							}
-							else if (Line.contains("name: "))
-							{
-								Name = Line.substring(Line.indexOf("name: ") + 6, Line.indexOf(";"));
-							}
-							else if (Line.contains("fog: "))
-							{
-								Fog = Line.substring(Line.indexOf("fog: ") + 5, Line.indexOf(";"));
-							}
-							else if (Line.contains("sky: "))
-							{
-								Sky = Line.substring(Line.indexOf("sky: ") + 5, Line.indexOf(";"));
-							}
-						}
-					}
-					else if (Line.contains("wall") || Line.contains("floor") || Line.contains("ceiling") || Line.contains("plane") || Line.contains("slope"))
-					{
-						Planes.add(new Plane());
-						boolean NameIsSet = false;
-
-						while (!Line.contains("}"))
-						{
-							Line = LevelFile.readLine();
-
-							if (Line.contains("{"))
-							{
-								continue;
-							}
-							else if (Line.contains("x: "))
-							{
-								Planes.get(Planes.size() - 1).AddVertex(Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";"))));
-							}
-							else if (Line.contains("y: "))
-							{
-								Planes.get(Planes.size() - 1).AddVertex(Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";"))));
-							}
-							else if (Line.contains("z: "))
-							{
-								Planes.get(Planes.size() - 1).AddVertex(Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";"))));
-							}
-							else if (Line.contains("2sided: "))
-							{
-								if ((Line.substring(Line.indexOf("2sided: ") + 8, Line.indexOf(";"))).equals("false"))
-								{
-									Planes.get(Planes.size() - 1).TwoSided(false);
-								}
-							}
-							else if (Line.contains("impassable: "))
-							{
-								if ((Line.substring(Line.indexOf("impassable: ") + 12, Line.indexOf(";"))).equals("false"))
-								{
-									Planes.get(Planes.size() - 1).Impassable(false);
-								}
-							}
-							else if (Line.contains("texture: "))
-							{
-								Planes.get(Planes.size() - 1).SetTextureName(Line.substring(Line.indexOf("texture: ") + 9, Line.indexOf(";")));
-								// The following line loads the textures if it needs to and sets a reference to it inside the plane.
-								Planes.get(Planes.size() - 1).SetReference(LoadTexture("textures/" + Planes.get(Planes.size() - 1).TextureName));
-								NameIsSet = true;
-							}
-							else if (Line.contains("light: "))
-							{
-								Planes.get(Planes.size() - 1).Lightning(Integer.parseInt(Line.substring(Line.indexOf("light: ") + 7, Line.indexOf(";"))));
-							}
-						}
-
-						// Calculate the plane's flat size
-						Planes.get(Planes.size() - 1).FlatLength = Planes.get(Planes.size() - 1).CalculateMaxFlatLength();
-						if (Planes.get(Planes.size() - 1).FlatLength < ShortestWall && Planes.get(Planes.size() - 1).FlatLength != 0)
-						{
-							ShortestWall = Planes.get(Planes.size() - 1).FlatLength;
-						}
-
-						if (!NameIsSet)
-						{
-							Planes.get(Planes.size() - 1).SetReference(LoadTexture("textures/DOOR9_1.bmp"));
-						}
-					}
-					else if (Line.contains("spawn"))
-					{
-						int PosX = 0;
-						int PosY = 0;
-						int PosZ = 0;
-						short Angle = 0;
-
-						while (!Line.contains("}"))
-						{
-							Line = LevelFile.readLine();
-
-							if (Line.contains("{"))
-							{
-								continue;
-							}
-							else if (Line.contains("x: "))
-							{
-								PosX = Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("y: "))
-							{
-								PosY = Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("z: "))
-							{
-								PosZ = Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("angle: "))
-							{
-								Angle = Short.parseShort(Line.substring(Line.indexOf("angle: ") + 7, Line.indexOf(";")));
-							}
-							else if (Line.contains("}"))
-							{
-								break;
-							}
-							else
-							{
-								System.out.println("Invalid property: " + Line);
-							}
-						}
-
-						Things.add(new Thing("Spawn", PosX, PosY, PosZ, Angle));
-						Spawns.add(new Thing("Spawn", PosX, PosY, PosZ, Angle));
-					}
-					else if (Line.contains("thing"))
-					{
-						int PosX = 0;
-						int PosY = 0;
-						int PosZ = 0;
-						short Angle = 0;	// Doesn't matter because the thing is a sprite
-						String Type = "";
-						boolean Impassable = true;
-
-						while (!Line.contains("}"))
-						{
-							Line = LevelFile.readLine();
-
-							if (Line.contains("{"))
-							{
-								continue;
-							}
-							else if (Line.contains("x: "))
-							{
-								PosX = Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("y: "))
-							{
-								PosY = Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("z: "))
-							{
-								PosZ = Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("angle: "))
-							{
-								Angle = Short.parseShort(Line.substring(Line.indexOf("angle: ") + 7, Line.indexOf(";")));
-							}
-							else if (Line.contains("type: "))
-							{
-								Type = Line.substring(Line.indexOf("type: ") + 6, Line.indexOf(";"));
-							}
-							else if (Line.contains("impassable: "))
-							{
-								Impassable = Boolean.parseBoolean(Line.substring(Line.indexOf("impassable: ") + 12, Line.indexOf(";")));
-							}
-							else if (Line.contains("}"))
-							{
-								break;
-							}
-							else
-							{
-								System.out.println("Invalid property: " + Line);
-							}
-						}
-
-						Things.add(new Thing(Type, PosX, PosY, PosZ));
-					}
-					else if (Line.contains("palmtree") || Line.contains("smallpalmtree"))
-					{
-						String Sprite = "";
-						int PosX = 0;
-						int PosY = 0;
-						int PosZ = 0;
-						int Light = 0;
-						int Radius = 16;
-						int Height = 96;
-						int Health = 100;
-						boolean Impassable = true;
-
-						while (!Line.contains("}"))
-						{
-							Line = LevelFile.readLine();
-
-							if (Line.contains("{"))
-							{
-								continue;
-							}
-							else if (Line.contains("texture: "))
-							{
-								Sprite = Line.substring(Line.indexOf("texture: ") + 9, Line.indexOf(";"));
-							}
-							else if (Line.contains("sprite: "))
-							{
-								Sprite = Line.substring(Line.indexOf("sprite: ") + 8, Line.indexOf(";"));
-							}
-							else if (Line.contains("x: "))
-							{
-								PosX = Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("y: "))
-							{
-								PosY = Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("z: "))
-							{
-								PosZ = Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";")));
-							}
-							else if (Line.contains("light: "))
-							{
-								Light = Integer.parseInt(Line.substring(Line.indexOf("light: ") + 7, Line.indexOf(";")));
-							}
-							else if (Line.contains("radius: "))
-							{
-								Radius = Integer.parseInt(Line.substring(Line.indexOf("radius: ") + 8, Line.indexOf(";")));
-							}
-							else if (Line.contains("height: "))
-							{
-								Height = Integer.parseInt(Line.substring(Line.indexOf("height: ") + 8, Line.indexOf(";")));
-							}
-							else if (Line.contains("health: "))
-							{
-								Health = Integer.parseInt(Line.substring(Line.indexOf("health: ") + 8, Line.indexOf(";")));
-							}
-							else if (Line.contains("impassable: "))
-							{
-								Impassable = Boolean.parseBoolean(Line.substring(Line.indexOf("impassable: ") + 12, Line.indexOf(";")));
-							}
-							else if (Line.contains("}"))
-							{
-								break;
-							}
-							else
-							{
-								System.err.println("Invalid property: " + Line);
-							}
-						}
-
-						Things.add(new Thing("res/sprites/" + Sprite, PosX, PosY, PosZ, Light, Radius, Height, Health));
-					}
-					else
-					{
-						System.err.println("Unknown data: " + Line);
+						Position = Temp;
 					}
 				}
-			}
-			catch (IOException ioe)
-			{
-				System.err.println(ioe.getStackTrace());
+
+				String Content = Line.substring(0, Position);
+				String id;
+				int Number;
+
+				if (Line.contains("level:"))
+				{
+					while (!Line.contains("}"))
+					{
+						Line = LevelFile.readLine();
+
+						if (Line.contains("{"))
+						{
+							continue;
+						}
+						else if (Line.contains("name: "))
+						{
+							Name = Line.substring(Line.indexOf("name: ") + 6, Line.indexOf(";"));
+						}
+						else if (Line.contains("fog: "))
+						{
+							Fog = Line.substring(Line.indexOf("fog: ") + 5, Line.indexOf(";"));
+						}
+						else if (Line.contains("sky: "))
+						{
+							Sky = Line.substring(Line.indexOf("sky: ") + 5, Line.indexOf(";"));
+						}
+					}
+				}
+				else if (Line.contains("wall") || Line.contains("floor") || Line.contains("ceiling") || Line.contains("plane") || Line.contains("slope"))
+				{
+					Planes.add(new Plane());
+					boolean NameIsSet = false;
+
+					while (!Line.contains("}"))
+					{
+						Line = LevelFile.readLine();
+
+						if (Line.contains("{"))
+						{
+							continue;
+						}
+						else if (Line.contains("x: "))
+						{
+							Planes.get(Planes.size() - 1).AddVertex(Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";"))));
+						}
+						else if (Line.contains("y: "))
+						{
+							Planes.get(Planes.size() - 1).AddVertex(Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";"))));
+						}
+						else if (Line.contains("z: "))
+						{
+							Planes.get(Planes.size() - 1).AddVertex(Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";"))));
+						}
+						else if (Line.contains("2sided: "))
+						{
+							if ((Line.substring(Line.indexOf("2sided: ") + 8, Line.indexOf(";"))).equals("false"))
+							{
+								Planes.get(Planes.size() - 1).TwoSided(false);
+							}
+						}
+						else if (Line.contains("impassable: "))
+						{
+							if ((Line.substring(Line.indexOf("impassable: ") + 12, Line.indexOf(";"))).equals("false"))
+							{
+								Planes.get(Planes.size() - 1).Impassable(false);
+							}
+						}
+						else if (Line.contains("texture: "))
+						{
+							Planes.get(Planes.size() - 1).SetTextureName(Line.substring(Line.indexOf("texture: ") + 9, Line.indexOf(";")));
+							// The following line loads the textures if it needs to and sets a reference to it inside the plane.
+							Planes.get(Planes.size() - 1).SetReference(LoadTexture("textures/" + Planes.get(Planes.size() - 1).TextureName));
+							NameIsSet = true;
+						}
+						else if (Line.contains("light: "))
+						{
+							Planes.get(Planes.size() - 1).Lightning(Integer.parseInt(Line.substring(Line.indexOf("light: ") + 7, Line.indexOf(";"))));
+						}
+					}
+
+					// Calculate the plane's flat size
+					Planes.get(Planes.size() - 1).FlatLength = Planes.get(Planes.size() - 1).CalculateMaxFlatLength();
+					if (Planes.get(Planes.size() - 1).FlatLength < ShortestWall && Planes.get(Planes.size() - 1).FlatLength != 0)
+					{
+						ShortestWall = Planes.get(Planes.size() - 1).FlatLength;
+					}
+
+					if (!NameIsSet)
+					{
+						Planes.get(Planes.size() - 1).SetReference(LoadTexture("textures/DOOR9_1.bmp"));
+					}
+				}
+				else if (Line.contains("spawn"))
+				{
+					int PosX = 0;
+					int PosY = 0;
+					int PosZ = 0;
+					short Angle = 0;
+
+					while (!Line.contains("}"))
+					{
+						Line = LevelFile.readLine();
+
+						if (Line.contains("{"))
+						{
+							continue;
+						}
+						else if (Line.contains("x: "))
+						{
+							PosX = Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("y: "))
+						{
+							PosY = Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("z: "))
+						{
+							PosZ = Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("angle: "))
+						{
+							Angle = Short.parseShort(Line.substring(Line.indexOf("angle: ") + 7, Line.indexOf(";")));
+						}
+						else if (Line.contains("}"))
+						{
+							break;
+						}
+						else
+						{
+							System.out.println("Invalid property: " + Line);
+						}
+					}
+
+					Things.add(new Thing("Spawn", PosX, PosY, PosZ, Angle));
+					Spawns.add(new Thing("Spawn", PosX, PosY, PosZ, Angle));
+				}
+				else if (Line.contains("thing"))
+				{
+					int PosX = 0;
+					int PosY = 0;
+					int PosZ = 0;
+					short Angle = 0;	// Doesn't matter because the thing is a sprite
+					String Type = "";
+					boolean Impassable = true;
+
+					while (!Line.contains("}"))
+					{
+						Line = LevelFile.readLine();
+
+						if (Line.contains("{"))
+						{
+							continue;
+						}
+						else if (Line.contains("x: "))
+						{
+							PosX = Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("y: "))
+						{
+							PosY = Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("z: "))
+						{
+							PosZ = Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("angle: "))
+						{
+							Angle = Short.parseShort(Line.substring(Line.indexOf("angle: ") + 7, Line.indexOf(";")));
+						}
+						else if (Line.contains("type: "))
+						{
+							Type = Line.substring(Line.indexOf("type: ") + 6, Line.indexOf(";"));
+						}
+						else if (Line.contains("impassable: "))
+						{
+							Impassable = Boolean.parseBoolean(Line.substring(Line.indexOf("impassable: ") + 12, Line.indexOf(";")));
+						}
+						else if (Line.contains("}"))
+						{
+							break;
+						}
+						else
+						{
+							System.out.println("Invalid property: " + Line);
+						}
+					}
+
+					Things.add(new Thing(Type, PosX, PosY, PosZ));
+				}
+				else if (Line.contains("palmtree") || Line.contains("smallpalmtree"))
+				{
+					String Sprite = "";
+					int PosX = 0;
+					int PosY = 0;
+					int PosZ = 0;
+					int Light = 0;
+					int Radius = 16;
+					int Height = 96;
+					int Health = 100;
+					boolean Impassable = true;
+
+					while (!Line.contains("}"))
+					{
+						Line = LevelFile.readLine();
+
+						if (Line.contains("{"))
+						{
+							continue;
+						}
+						else if (Line.contains("texture: "))
+						{
+							Sprite = Line.substring(Line.indexOf("texture: ") + 9, Line.indexOf(";"));
+						}
+						else if (Line.contains("sprite: "))
+						{
+							Sprite = Line.substring(Line.indexOf("sprite: ") + 8, Line.indexOf(";"));
+						}
+						else if (Line.contains("x: "))
+						{
+							PosX = Integer.parseInt(Line.substring(Line.indexOf("x: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("y: "))
+						{
+							PosY = Integer.parseInt(Line.substring(Line.indexOf("y: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("z: "))
+						{
+							PosZ = Integer.parseInt(Line.substring(Line.indexOf("z: ") + 3, Line.indexOf(";")));
+						}
+						else if (Line.contains("light: "))
+						{
+							Light = Integer.parseInt(Line.substring(Line.indexOf("light: ") + 7, Line.indexOf(";")));
+						}
+						else if (Line.contains("radius: "))
+						{
+							Radius = Integer.parseInt(Line.substring(Line.indexOf("radius: ") + 8, Line.indexOf(";")));
+						}
+						else if (Line.contains("height: "))
+						{
+							Height = Integer.parseInt(Line.substring(Line.indexOf("height: ") + 8, Line.indexOf(";")));
+						}
+						else if (Line.contains("health: "))
+						{
+							Health = Integer.parseInt(Line.substring(Line.indexOf("health: ") + 8, Line.indexOf(";")));
+						}
+						else if (Line.contains("impassable: "))
+						{
+							Impassable = Boolean.parseBoolean(Line.substring(Line.indexOf("impassable: ") + 12, Line.indexOf(";")));
+						}
+						else if (Line.contains("}"))
+						{
+							break;
+						}
+						else
+						{
+							System.err.println("Invalid property: " + Line);
+						}
+					}
+
+					Things.add(new Thing("res/sprites/" + Sprite, PosX, PosY, PosZ, Light, Radius, Height, Health));
+				}
+				else
+				{
+					System.err.println("Unknown data: " + Line);
+				}
 			}
 		}
 		catch (FileNotFoundException fnfe)
@@ -349,10 +342,16 @@ public class Level
 			System.err.println("The specified level cannot be found: " + LvlName);
 			System.exit(1);
 		}
+		catch (IOException ioe)
+		{
+			System.err.println(ioe.getStackTrace());
+			System.exit(1);
+		}
 		catch (Exception e)
 		{
 			System.err.println("Failed to load the following level: " + LvlName);
 			System.err.println("The must be a syntax error in the file (missing closing accolade, colon, semicolon, etc.)");
+			System.err.println("The error occurred on the following line: \"" + Line + "\"");
 			System.exit(1);
 		}
 	}
